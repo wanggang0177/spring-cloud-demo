e3ee843bb1a952e2d7286ae5ff21e01e6e40d8d7
wanggang <15002477523@163.com>
2018-07-16 09:58:29 +0800

    链路

---
+++ zipkin-server/src/test/java/eurekaserver/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,11 @@
+package eurekaserver.eurekaserver;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplicationTests {
+
+	public void contextLoads() {
+	}
+
+}
